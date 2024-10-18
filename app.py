148d3f250867bfd2b33e334ab806c6d99c81675c
Uzayr Iqbal Hamid <hamiduzayr@gmail.com>
2024-10-18 18:38:29 +0530

    added the wide mode, as it was not added before

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ with col1:
 # Adding a divider between two columns
 with col2:
     st.markdown(
-        "<div style='height: 100%; width: 1px; background-color: black; margin: auto;'></div>",
+        "<div style='height: 100%; width: 10px; background-color: black; margin: auto;'></div>",
         unsafe_allow_html=True
     )
 
@@ -40,4 +40,25 @@ with col2:
 # Add content to the third column
 with col3:
     st.markdown("<h2 style='text-align: center;'>Code</h2>", unsafe_allow_html=True)
-    st.write("This is the content in the second column.")
+    st.code(
+        """
+        import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import to_categorical
+
+from sklearn.metrics import confusion_matrix , classification_report 
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import roc_curve, auc, roc_auc_score
+
+from IPython.display import clear_output
+import warnings
+warnings.filterwarnings('ignore')
+        """
+    )
