e651953299b3a60185228e2176898d47accc6395
Uzayr Iqbal Hamid <hamiduzayr@gmail.com>
2024-10-18 18:28:47 +0530

    added .streamlit/config.toml file, created columns separating webcam column and code column

--- app.py
+++ app.py
@@ -19,4 +19,25 @@ from IPython.display import clear_output
 import warnings
 warnings.filterwarnings('ignore')
 
-st.write("hi")
+import streamlit as st
+
+# Create two columns
+col1, col2, col3 = st.columns([4, 1, 4])
+
+# Add content to the first column
+with col1:
+    st.markdown("<h2 style='text-align: center;'>Working Model</h2>", unsafe_allow_html=True)
+    st.write("webcam here")
+
+# Adding a divider between two columns
+with col2:
+    st.markdown(
+        "<div style='height: 100%; width: 1px; background-color: black; margin: auto;'></div>",
+        unsafe_allow_html=True
+    )
+
+
+# Add content to the third column
+with col3:
+    st.markdown("<h2 style='text-align: center;'>Code</h2>", unsafe_allow_html=True)
+    st.write("This is the content in the second column.")
